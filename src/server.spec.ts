b60dac1f1a210aa347d77f7929e60d1972d757af
Sam Zagrobelny <strangesast@gmail.com>
2019-12-30 16:13:53 -0500

    first

---
+++ src/server.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import "mocha";
+
+describe("this", () => {
+  describe("should", () => {
+    it("always pass", () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 3000 }, () =>
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+);
